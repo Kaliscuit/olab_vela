737e4e772260a1974e9cd90420fb8361bfc767cd
xiaozhui <yuhanjian@yahoo.com>
2013-07-20 07:58:42 +0000

    rem swp

--- model/snap.py
+++ model/snap.py
@@ -9,7 +9,9 @@ class Snap():
     '''
 
     def __init__(self,db,id):
-     self._db = db
+        self._db = db
+        snap = self.get_snap(id)
+
     @property
     def db(self):
         return self._db
@@ -30,7 +32,19 @@ class Snap():
     def members(self):
         return self._contents
 
-    def get_snap(self):
+    def get_snap(self,id):
+        self._id = id
+        snap = self.db.snap.find_one({'id' : self.id})
+        if id:
+            self._snap_time = snap['snap_time']
+            self._creator = snap['creator']
+            self._members = snap['members']
+            self._contents = snap['continue']
+        else:
+            self._snap_time = ''
+            self._creator = ''
+            self._members = ''
+            self._conetents = ''
 
-    
+            
 
