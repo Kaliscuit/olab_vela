7ff4246505ec6840d45ac5275312d8cfb57d95fa
xiaozhui <yuhanjian@yahoo.com>
2013-07-20 19:08:17 +0800

    push

--- main.py
+++ main.py
@@ -15,30 +15,31 @@ def hello_world():
 
 @app.route('/user/register', methods=['POST'])
 def user_register():
-    try:
-        if request.form['email'].strip() and request.form['password'] and request.form['nickname'].strip():
-            user_email = request.form['email'].strip()
-            user_password = request.form['password']
-            user_nickname = request.form['nickname'].strip()
-            if is_email(user_email):
-                exist_user = user.User({'email': user_email})
-                if exist_user.password:
-                    ec = 1062
-                    em = 'Email already exists'
-                else:
-                    user_id = guid.new('user')
-                    new_user = user.User({'id': user_id})
-                    m = md5()         
-                    m.update(request.form['password'])
-                    new_user.password = m.hexdigest()
-                    new_user.email = user_email
-                    new_user.nickname = user_nickname
-                    new_user.save()
-                    ec = 200
-                    em = 'ok'
+    if request.form['email'].strip() and request.form['password'] and request.form['nickname'].strip():
+        user_email = request.form['email'].strip()
+        user_password = request.form['password']
+        user_nickname = request.form['nickname'].strip()
+        if is_email(user_email):
+            exist_user = user.User({'email': user_email})
+            if exist_user.password:
+                ec = 1062
+                em = 'Email already exists'
             else:
-                ec = 417
-                em = 'Invalid email'
+                user_id = guid.new('user')
+                new_user = user.User({'id': user_id})
+                m = md5()         
+                m.update(request.form['password'])
+                new_user.password = m.hexdigest()
+                new_user.email = user_email
+                new_user.nickname = user_nickname
+                new_user.save()
+                ec = 200
+                em = 'ok'
+        else:
+            ec = 417
+            em = 'Invalid email'
+    try:
+        2 < 1
     except:
         ec = 500
         em = 'Post data error'
