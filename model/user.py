12fe95466907d0e2e1d9130cd1150a56ef78f9b0
xiaozhui <yuhanjian@yahoo.com>
2013-07-20 16:22:43 +0800

    user

--- model/user.py
+++ model/user.py
@@ -3,7 +3,6 @@ import datetime
 
 
 class User():
-
     '''
     User Model
     Base properties and functions
@@ -13,40 +12,33 @@ class User():
         self._db = db
         user = self.get_user(user_email)
 
-
     @property
     def db(self):
         return self._db
-
 
     @property
     def nickname(self):
         return self._nickname
 
-
     @property
     def email(self):
         return self._email
-
 
     @property
     def password(self):
         return self._password
 
-
     @property
     def register_time(self):
         return self._register_time
-
 
     @property
     def update_time(self):
         return self._update_time
 
-
     def get_user(self, user_email):
         self._email = user_email
-        user = self.db.user.find_one({'email' : self.email})
+        user = self.db.user.find_one({'email': self.email})
         if user:
             self._nickname = user['nickname']
             self._password = user['password']
@@ -58,12 +50,16 @@ class User():
             self._register_time = ''
             self._update_time = ''
 
-    def save(self):
+    def get_properties(self):
         properties = {
-                'nickname':self.nickname,
-                'email':self.email,
-                'password':self.password,
-                'register_time':datetime.datetime.utcnow(),
-                'update_time':datetime.datetime.utcnow()
-                }
-        self.db.user.update({'email' : self.email}, properties, upsert=True)
+            'nickname': self.nickname,
+            'email': self.email,
+            'password': self.password,
+            'register_time': datetime.datetime.utcnow(),
+            'update_time': datetime.datetime.utcnow()
+        }
+        return properties
+
+    def save(self):
+        properties = self.get_properties()
+        self.db.user.update({'email': self.email}, properties, upsert=True)
---
+++ vela.py
@@ -0,0 +1,56 @@
+# /usr/bin/env python
+import datetime
+
+
+class Vela():
+
+    '''
+    Vela Model
+    Base properties and functions
+    '''
+
+    def __init__(self, db):
+        self._db = db
+        self._owner = ''
+        self._members = ''
+        self._routes = ''
+        self._create_time = ''
+
+
+    @property
+    def db(self):
+        return self._db
+
+
+    @property
+    def owner(self):
+        return self._owner
+
+
+    @property
+    def members(self):
+        return self._members
+
+
+    @property
+    def routes(self):
+        return self._routes
+
+
+    @property
+    def create_time(self):
+        return self._create_time
+
+
+    def create(self):
+        properties = {
+                'owner':self._owner,
+                'members':self._members,
+                'routes':self._routes,
+                'create_time':datetime.datetime.utnow()
+                }
+        db.vela.insert(properties)
+
+
+    def add_snap_to_route(self, snap_id):
+        self._routes.append(snap_id)
